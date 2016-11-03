78160e1ef93ba59b68701ba5cadf6fcec8a61078
Norberto Vasconcelos <n.g.vasconcelos@gmail.com>
2016-11-03 16:09:38 +0000

    * Main.axml: Layout improvements.
    * Resource.designer.cs:
    * TodoListItem.axml:

--- Droid/Resources/Resource.designer.cs
+++ Droid/Resources/Resource.designer.cs
@@ -88,29 +88,32 @@ namespace ToDo.Droid
 		public partial class Id
 		{
 			
-			// aapt resource value: 0x7f080003
-			public const int buttonClearCompleted = 2131230723;
+			// aapt resource value: 0x7f080004
+			public const int buttonClearCompleted = 2131230724;
 			
-			// aapt resource value: 0x7f080007
-			public const int buttonDelete = 2131230727;
-			
-			// aapt resource value: 0x7f080005
-			public const int checkBoxCompleted = 2131230725;
-			
-			// aapt resource value: 0x7f080001
-			public const int checkBoxSelectAll = 2131230721;
-			
-			// aapt resource value: 0x7f080000
-			public const int contianerCreateToDo = 2131230720;
+			// aapt resource value: 0x7f080008
+			public const int buttonDelete = 2131230728;
 			
 			// aapt resource value: 0x7f080006
-			public const int editTextName = 2131230726;
+			public const int checkBoxCompleted = 2131230726;
 			
 			// aapt resource value: 0x7f080002
-			public const int editTextToDo = 2131230722;
+			public const int checkBoxSelectAll = 2131230722;
 			
-			// aapt resource value: 0x7f080004
-			public const int listview = 2131230724;
+			// aapt resource value: 0x7f080001
+			public const int contianerCreateToDo = 2131230721;
+			
+			// aapt resource value: 0x7f080007
+			public const int editTextName = 2131230727;
+			
+			// aapt resource value: 0x7f080003
+			public const int editTextToDo = 2131230723;
+			
+			// aapt resource value: 0x7f080005
+			public const int listview = 2131230725;
+			
+			// aapt resource value: 0x7f080000
+			public const int tvActiveItems = 2131230720;
 			
 			static Id()
 			{
